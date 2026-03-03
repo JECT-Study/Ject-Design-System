3bc3446e191a07746bd39beb3d311c2026b8c02f
sohee <106158901+ccconac@users.noreply.github.com>
2026-03-03 16:20:10 +0900

    chore: 기초 환경 구축 (#3)
    * docs: PR template 추가
    
    * remove: 불필요한 파일 삭제
    
    * chore: 기본 환경 설정 및 코드 포맷팅 추가
    
    * chore: mdx 관련 패키지 및 jds 설치
    
    ---------
    
    Co-authored-by: BANG JAEHYUN <95038747+CRITICBANGGU@users.noreply.github.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+const eslintConfig = [
+  ...nextVitals,
+  ...nextTs,
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
+  {
+    ignores: ['.next/*', 'node_modules/*', '_data/*'],
+  },
+  eslintConfigPrettier,
+];
+
+export default eslintConfig;

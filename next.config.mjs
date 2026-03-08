f5d2973bee601476b8b3365d4ecb94340569f669
JaeHyun Bang <jhbang@plea.kr>
2026-03-08 20:19:13 +0900

    chore : next config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
-import type { NextConfig } from 'next';
 import createMDX from '@next/mdx';
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 };
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [['rehype-slug']],
+  },
 });
 
 export default withMDX(nextConfig);

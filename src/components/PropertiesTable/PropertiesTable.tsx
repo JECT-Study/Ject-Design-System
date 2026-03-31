582f9541650519838a7902cc55df7a790b068080
ccconac <qwerpoiu91@naver.com>
2026-04-01 01:17:14 +0900

    fix: Table component에 rounded 속성이 적용되지 않는 이슈 수정

--- src/components/PropertiesTable/PropertiesTable.tsx
+++ src/components/PropertiesTable/PropertiesTable.tsx
@@ -7,30 +7,30 @@ import { PropertiesTableProps } from './PropertiesTable.type';
 export function PropertiesTable({ rows }: PropertiesTableProps) {
   return (
     <div className="flex flex-col gap-24">
-      <Table.Root>
-        <Table.Header>
-          <Table.Row>
+      <div className="rounded-6 overflow-hidden">
+        <Table.Root>
+          <Table.Header>
             <Table.HeaderItem>이름</Table.HeaderItem>
             <Table.HeaderItem>기본값</Table.HeaderItem>
             <Table.HeaderItem>타입</Table.HeaderItem>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {rows.map((row) => (
-            <Table.Row key={row.name}>
-              <Table.RowItem variant="label" description={row.description}>
-                {row.name}
-              </Table.RowItem>
-              <Table.RowItem variant="code">{row.default ?? '-'}</Table.RowItem>
-              <Table.RowItem variant="code">
-                {row.type.map((rowType) => (
-                  <span key={rowType}>{rowType}</span>
-                ))}
-              </Table.RowItem>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+          </Table.Header>
+          <Table.Body>
+            {rows.map((row) => (
+              <Table.Row key={row.name}>
+                <Table.RowItem variant="label" description={row.description}>
+                  {row.name}
+                </Table.RowItem>
+                <Table.RowItem variant="code">{row.default ?? '-'}</Table.RowItem>
+                <Table.RowItem variant="code">
+                  {row.type.map((rowType) => (
+                    <span key={rowType}>{rowType}</span>
+                  ))}
+                </Table.RowItem>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+      </div>
     </div>
   );
 }
